6d39742b3bc8535d50c5fd86441a72421502d15b
Bruno Campos <bruno05campos11@gmail.com>
2024-08-05 17:53:50 -0300

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,9 @@
+from googleapiclient.discovery import build
+chave_api = "AIzaSyB0kCjM91ldEDJ4kfGwtqWPGnhKkjG1ASc"
+youtube = build('youtube','v3', developerKey=chave_api)
+import json
+
+pesquisa = youtube.search().list (q="one piece crítica", part = 'snippet', maxResults = 5, type="playlist").execute()
+for item in pesquisa ['items']:
+  print (f"Título da playlist: {item ['snippet']['title']}")
+  print (f"Thumbnails: {item ['snippet']['thumbnails']}\n")
